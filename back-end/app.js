caf010453611580a46c22cc376b540050ad1c986
fgallou <florentgallou@gmail.com>
2022-10-19 14:57:35 +0200

    mongodb linked

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('DB is OK'))
+  .catch(() => console.log('DB failed'));
 
 // export one function that gets called once as the server is being initialized
 module.exports = function (app, server) {
