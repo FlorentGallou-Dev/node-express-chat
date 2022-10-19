0c871648752901b5ae8bb3114d5bd57c57f1c5f8
fgallou <florentgallou@gmail.com>
2022-10-19 16:19:56 +0200

    unable to receive emited message from main.js to app.js dont know why... stuck-_-'

--- front-end/main.js
+++ front-end/main.js
@@ -1,14 +1,22 @@
-(function () {
-    const server = 'http://127.0.0.1:3000'
-    const socket = io(server);
 
-    socket.on('notification', (data) => {
-        console.log('Message depuis le seveur:', data);
-    })
+const server = 'http://127.0.0.1:3000'
+const socket = io(server);
 
-    fetch(`${server}/test`).then((res) => {
-        return res.json()
-    }).then((data) => {
-        console.log(data);
-    })
-})()
+const send = document.getElementById('submitButton');
+send.addEventListener('click', () => {
+    const msg = document.getElementById('msg');
+
+    socket.emit('message-text', msg.value);
+
+    msg.value = '';
+});
+
+socket.on('notification', (data) => {
+    console.log('Message depuis le seveur:', data);
+})
+
+fetch(`${server}/test`).then((res) => {
+    return res.json()
+}).then((data) => {
+    console.log(data);
+})
